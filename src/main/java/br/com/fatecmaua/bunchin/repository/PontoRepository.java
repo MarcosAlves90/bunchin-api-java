15f2f2f7764054730178962e0ed7ae0c3be6b7fd
MarcosAlves90 <MarcosAlves25@proton.me>
2025-05-19 05:00:05 -0300

    fix PontoRepository.java

--- src/main/java/br/com/fatecmaua/bunchin/repository/PontoRepository.java
+++ src/main/java/br/com/fatecmaua/bunchin/repository/PontoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PontoRepository extends JpaRepository<Ponto, String> {
+public interface PontoRepository extends JpaRepository<Ponto, java.util.UUID> {
 }
