5f2422cb7107847dd5fb620985260a13032ac19e
Matheus Souza Rodrigues <79955422+MatheusGaviota@users.noreply.github.com>
2025-06-07 03:26:19 +0000

    Adiciona importações de Funcionario, List e Instant no PontoRepository

--- src/main/java/br/com/fatecmaua/bunchin/repository/PontoRepository.java
+++ src/main/java/br/com/fatecmaua/bunchin/repository/PontoRepository.java
@@ -1,8 +1,11 @@
 package br.com.fatecmaua.bunchin.repository;
 
 import br.com.fatecmaua.bunchin.model.Ponto;
+import br.com.fatecmaua.bunchin.model.Funcionario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.time.Instant;
 
 @Repository
 public interface PontoRepository extends JpaRepository<Ponto, java.util.UUID> {
