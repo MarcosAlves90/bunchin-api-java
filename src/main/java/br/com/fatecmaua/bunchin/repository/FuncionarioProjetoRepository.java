fa92b9e884532f907b1e510d86c6935292dbbc78
Matheus Souza Rodrigues <79955422+MatheusGaviota@users.noreply.github.com>
2025-06-10 04:33:24 +0000

    Atualiza método de busca por funcionário no repositório FuncionarioProjeto para usar consulta JPQL com parâmetros nomeados

--- src/main/java/br/com/fatecmaua/bunchin/repository/FuncionarioProjetoRepository.java
+++ src/main/java/br/com/fatecmaua/bunchin/repository/FuncionarioProjetoRepository.java
@@ -3,11 +3,14 @@ package br.com.fatecmaua.bunchin.repository;
 import br.com.fatecmaua.bunchin.model.FuncionarioProjeto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 @Repository
 public interface FuncionarioProjetoRepository extends JpaRepository<FuncionarioProjeto, Integer> {
-    List<FuncionarioProjeto> findByFuncionarioN_registro(Integer funcionarioId);
+    @Query("SELECT fp FROM FuncionarioProjeto fp WHERE fp.funcionario.n_registro = :nRegistro")
+    List<FuncionarioProjeto> findByFuncionarioNRegistro(@Param("nRegistro") Integer nRegistro);
     List<FuncionarioProjeto> findByProjetoIdProjeto(Integer projetoId);
     List<FuncionarioProjeto> findByStatus(String status);
 }
