50934f069efebf9d0a0dce3f30c6a25f482d3207
Matheus Souza Rodrigues <79955422+MatheusGaviota@users.noreply.github.com>
2025-06-10 04:41:47 +0000

    Atualiza método de busca por responsável no repositório ProjetoRepository para usar consulta JPQL com parâmetros nomeados

--- src/main/java/br/com/fatecmaua/bunchin/repository/ProjetoRepository.java
+++ src/main/java/br/com/fatecmaua/bunchin/repository/ProjetoRepository.java
@@ -2,13 +2,18 @@ package br.com.fatecmaua.bunchin.repository;
 
 import br.com.fatecmaua.bunchin.model.Projeto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
 public interface ProjetoRepository extends JpaRepository<Projeto, Integer> {
     List<Projeto> findByOrganizacaoIdOrganizacao(Integer organizacaoId);
-    List<Projeto> findByResponsavelN_registro(Integer responsavelId);
+    
+    @Query("SELECT p FROM Projeto p WHERE p.responsavel.n_registro = :responsavelId")
+    List<Projeto> findByResponsavelNRegistro(@Param("responsavelId") Integer responsavelId);
+    
     List<Projeto> findByStatus(String status);
     List<Projeto> findByNomeContainingIgnoreCase(String nome);
 }
