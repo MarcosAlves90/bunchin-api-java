ab9dc1b003618aa02719d56574c483b3ed3e7125
Matheus Souza Rodrigues <79955422+MatheusGaviota@users.noreply.github.com>
2025-06-14 03:31:56 +0000

    Atualiza método de busca por registro no repositório FuncionarioRepository para retornar uma lista de objetos Funcionario e corrige a anotação de consulta.

--- src/main/java/br/com/fatecmaua/bunchin/repository/FuncionarioRepository.java
+++ src/main/java/br/com/fatecmaua/bunchin/repository/FuncionarioRepository.java
@@ -10,7 +10,8 @@ import java.util.List;
 
 @Repository
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Integer> {
-    Optional<Funcionario> findByNRegistro(Integer n_registro);    
+    @Query("SELECT * FROM tb_funcionario fp WHERE n_registro = :n_registro")
+    List<Funcionario> findByNRegistro(@Param("n_registro") Integer n_registro);
     Optional<Funcionario> findByCpf(String cpf);
     Optional<Funcionario> findByEmail(String email);
     Optional<Funcionario> findByEmailOrCpf(String email, String cpf);
